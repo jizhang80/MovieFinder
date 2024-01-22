b63a20e981982bed8780eca0b39b02c7abec6c12
AnthoG <anthony.dswg@gmail.com>
2024-01-22 00:32:57 -0500

    User Authentication Backend and User Model and Adjustment to reslvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,16 @@
-const { Movie } = require('../models');
+const { Movie, User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');  //A.G Added
 
 const resolvers = {
   Query:{
+
+    users: async () => {
+      return User.find().populate('movies'); //Added by A.G 
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('movies'); //Added by A.G to Populate movies of the user
+    },
+
     movies: async () => {
       return Movie.find();
     },
@@ -19,11 +28,64 @@ const resolvers = {
   },
   
   Mutation: {
-    addMovie: async (parent, { title }) => {
-      return Movie.create({ title });
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    removeMovie: async (parent, { movieId }) => {
-      return Movie.findOneAndDelete({ _id: movieId });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+
+    // addMovie: async (parent, { title }) => {
+    //   return Movie.create({ title });
+    // },
+
+    addMovie: async (parent, { title }, context) => {
+      if (context.user) {
+        const movie = Movie.create({ title });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { movies: movie._id } }
+        );
+
+        return movie;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    // removeMovie: async (parent, { movieId }) => {
+    //   return Movie.findOneAndDelete({ _id: movieId });
+    // },
+
+    removeMovie: async (parent, { movieId }, context) => {
+      if (context.user){
+        const movie = Movie.findOneAndDelete({ _id: movieId });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { movies: movie._id } }
+        );
+        return movie;
+      }
+      throw new AuthenticationError('You need to be logged in!');    
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,13 @@
 
 const typeDefs = `
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  movies: [Movie]!
+}
   type Movie {
     _id: ID!
     title: String!
@@ -13,7 +21,14 @@ const typeDefs = `
     thumbnail_height: Int
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    users: [User]
+    user(username: String): User 
     movies: [Movie]!
     movie(_id: ID!): Movie
     searchMovies( keyword: String!): [Movie]
@@ -21,6 +36,8 @@ const typeDefs = `
 
   # Important for useMutation: We define our Mutation type to inform our entrypoints
   type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
     addMovie(title: String!): Movie
     removeMovie(_id: ID!): Movie
   }
