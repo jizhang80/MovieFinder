13171ca37ca73e65426d1fc625606f3969fa1282
bestedt <ty_bested@hotmail.com>
2024-01-22 20:21:27 -0500

    functional frontend

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,7 @@ import CompanyLogo from '../images/purpleturtle.png';
 const Footer = () => {
   return (
     <Box
+<<<<<<< Updated upstream
       component="footer"
       sx={{
         display: 'flex',
@@ -19,5 +20,20 @@ const Footer = () => {
     </Box>
   );
 };
+=======
+    component="footer"
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '80px', 
+      backgroundColor: '#f5f5f5', 
+      border: '2px solid red', 
+    }}
+  >
+    <img src={CompanyLogo} alt="Company Logo" style={{ height: '50px' }} />
+  </Box>
+)};
+>>>>>>> Stashed changes
 
 export default Footer;
