a35ba96437057f05af1812f17817a0febc4810df
jizhang80 <zhangji80@outlook.com>
2024-01-23 16:41:49 -0500

    add movie card component

---
+++ client/src/components/MovieCard.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/icons-material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+
+
+export default function MovieCard({movie}) {
+  // about movie image, please ref: https://developer.themoviedb.org/docs/image-basics
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  return (
+    <div>
+      <Link to={`/movie/${movie.id}`}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="194"
+            image={imageUrl}
+            alt={movie.title}
+          />
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+                  {movie.title}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </Link>
+    </div>
+  );
+}
