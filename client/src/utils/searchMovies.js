a1a48b85d9cd972f1af50ab0a3919d1cb300e515
jizhang80 <zhangji80@outlook.com>
2024-01-29 13:07:24 -0500

    show env for client

--- client/src/utils/searchMovies.js
+++ client/src/utils/searchMovies.js
@@ -9,6 +9,7 @@ const options = {
 
 
 export default async function searchMovie(keywords) {
+	console.log(import.meta.env.VITE_TMDB_TOKEN)
 	let movies = [];
 	const url = `https://api.themoviedb.org/3/search/movie?query=${keywords}&include_adult=false&language=en-US&page=1&region=CA`;
 	try {
